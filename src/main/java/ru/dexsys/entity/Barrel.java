9363a5398ba79f78474059191c586f5a2978af5c
Alexander <vyshky2@gmail.com>
2021-10-18 21:37:56 +0400

    ref

--- src/main/java/ru/dexsys/entity/Barrel.java
+++ src/main/java/ru/dexsys/entity/Barrel.java
@@ -31,6 +31,6 @@ public class Barrel {
         }
         return Paint.Cyan(alcohol +
                 ", size=" + size +
-                ", currentFiling=" + currentFiling) + '}';
+                ", currentFiling=" + currentFiling);
     }
 }
